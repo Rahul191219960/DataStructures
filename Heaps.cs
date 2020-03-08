9b12c95d86b754c6cb7fd265242355d63d2ffbcf
Rahul <rahulsingh191219960@gmail.com>
2020-03-09 00:03:29 +0530

    Recusrion to satisfy the max-heapp property at every node

--- Heaps.cs
+++ Heaps.cs
@@ -14,28 +14,49 @@ namespace Heaps
             }
             else
             {
-                if ((index * 2 + 1) < myEmp.Length && myEmp[index] <= myEmp[2 * index + 1])
+                if (((index * 2) + 2) < myEmp.Length)  // right child is present
                 {
-                    Employee temp = new Employee();
-                    temp = myEmp[2 * index + 1];
-                    myEmp[2 * index + 1] = myEmp[index];
-                    myEmp[index] = temp;
+                    Employee right = new Employee();
+                    Employee left = new Employee();
+                    // Employee largest = new Employee();
+                    int Largest;
+
+                    if (myEmp[index * 2 + 2] < myEmp[index * 2 + 1])
+                    {
+                        Largest = index * 2 + 1;
+                        Employee temp = new Employee();
+                        temp = myEmp[index * 2 + 1];
+                        myEmp[index * 2 + 1] = myEmp[index];
+                        myEmp[index] = temp;
+                    }
+                    else
+                    {
+                        Largest = index * 2 + 2;
+                        Employee temp = new Employee();
+                        temp = myEmp[index * 2 + 2];
+                        myEmp[index * 2 + 2] = myEmp[index];
+                        myEmp[index] = temp;
+                    }
+                    Max_Heapify(myEmp,Largest);
                 }
-                if (((index * 2) + 2) < myEmp.Length && myEmp[index] < myEmp[2 * index + 2])
+                else if ((index * 2 + 1) < myEmp.Length && myEmp[index] < myEmp[index * 2 + 1]) // only left child is present
                 {
+                    Employee left = new Employee();
+                    left = myEmp[index * 2 + 1];
                     Employee temp = new Employee();
-                    temp = myEmp[2 * index + 2] ;
-                    myEmp[2 * index + 2] = myEmp[index];
+                    temp = myEmp[index * 2 + 1];
+                    myEmp[index * 2 + 1] = myEmp[index];
                     myEmp[index] = temp;
                 }
             }
         }
 
-        public T Build_MaxHeap( T myEmp)
+
+        public T Build_MaxHeap(T myEmp)
         {
-            for(int i = myEmp.Length / 2 ; i >=0 ; i --)
+            for (int i = myEmp.Length / 2; i >= 0; i--)
             {
-                Max_Heapify(myEmp,i);
+                Max_Heapify(myEmp, i);
             }
             return myEmp;
         }
